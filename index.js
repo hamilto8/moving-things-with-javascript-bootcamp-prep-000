1f9b3b798d750ff3f5fa6bb4839030dbda6377a5
Michael Hamilton <michael.ts.hamilton@gmail.com>
2018-07-01 02:44:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,9 +8,26 @@ dodger.style.bottom = '0px';
 
 document.addEventListener('keydown', function(e) {
   if (e.which === 37) {
-    var leftNumbers = dodger.style.left.replace('px', '')
-    var left = parseInt(leftNumbers, 10)
-
-    dodger.style.left = `${left - 1}px`
+    moveDodgerLeft();
+  } else if(e.which === ) {
+    
   }
 })
+
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`
+  }
+}
+
+function moveDodgerRight() {
+  var rightNumbers = dodger.style.right.replace('px', '')
+  var right = parseInt(leftNumbers, 10)
+
+  if (right > 0) {
+    dodger.style.left = `${left + 1}px`
+  }
+}
