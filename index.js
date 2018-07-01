7ee2a7ee8d63de345217b3ef503b2fde89a62515
Michael Hamilton <michael.ts.hamilton@gmail.com>
2018-07-01 02:52:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,6 +28,6 @@ function moveDodgerRight() {
   var right = parseInt(rightNumbers, 10)
 
   if (right >= 360) {
-    dodger.style.right = `${right - 1}px`
+    dodger.style.left = `${right - 1}px`
   }
 }
